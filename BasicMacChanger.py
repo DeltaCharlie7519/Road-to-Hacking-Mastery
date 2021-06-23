8d1e720508977e71857f726af33e59864b68e541
abhiram1809 <53875874+abhiram1809@users.noreply.github.com>
2021-06-23 09:46:04 -0400

    Basic Mac Changer but with user input

--- BasicMacChanger.py
+++ BasicMacChanger.py
@@ -1,4 +1,5 @@
 import subprocess
+new_mac = input()
 subprocess.call("ifconfig eth0 down", shell=True)
-subprocess.call("ifconfig eth0 hw ether 11:22:33:44:55:66", shell=True)
+subprocess.call("ifconfig eth0 hw ether " + new_mac, shell=True)
 subprocess.call("ifconfig eth0 up", shell=True)
